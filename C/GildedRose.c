0980dd04685771fa6c53cccd24caa593d14a8480
zqtay <zqtay96@gmail.com>
2022-05-22 17:04:52 +0800

    Introduce item type handling

--- C/GildedRose.c
+++ C/GildedRose.c
@@ -1,6 +1,18 @@
 #include <string.h>
 #include "GildedRose.h"
 #include <stdio.h>
+#include <ctype.h>
+
+typedef struct {
+    const char* name;
+    int type;
+} ItemTypeReg;
+
+// Define custom item list with specific type
+// for items that do not contain item type keyword in their names
+ItemTypeReg itemTypeReg[1] = {
+    {"sulfuras", ITEM_TYPE_LEGENDARY}
+};
 
 Item*
 init_item(Item* item, const char *name, int sellIn, int quality)
@@ -18,81 +30,109 @@ print_item_to_buffer(char* buffer, Item* item)
     sprintf(buffer, "%s, %d, %d", item->name, item->sellIn, item->quality);
 }
 
+void toLowerCaseStr(char* str, char* buf) {
+    int i = 0;
+    for(; str[i]; i++){
+        buf[i] = tolower(str[i]);
+    }
+    buf[i] = 0x00;
+}
+
+int getItemType(Item item) {
+    int type = ITEM_TYPE_COMMON;
+    char lowerName[256];
+    toLowerCaseStr(item.name, lowerName);
+    if (strstr(lowerName, "aged") != NULL) {
+        type |= ITEM_TYPE_AGED;
+    }
+    if (strstr(lowerName, "legendary") != NULL) {
+        type |= ITEM_TYPE_LEGENDARY;
+    }
+    if (strstr(lowerName, "pass") != NULL) {
+        type |= ITEM_TYPE_PASS;
+    }
+    if (strstr(lowerName, "conjured") != NULL) {
+        type |= ITEM_TYPE_CONJURED;
+    }
+    for (int i = 0; i < (sizeof(itemTypeReg) / sizeof(itemTypeReg[0])); i++) {
+        if (strstr(lowerName, itemTypeReg[i].name) != NULL) {
+            type |= itemTypeReg[i].type;
+        }
+    }
+    return type;
+}
+
+int calcNextSellIn(Item item, int type){
+    if (type & ITEM_TYPE_LEGENDARY){
+        // Only legendary item does not affect sellIn value
+        return item.sellIn;
+    }
+    return item.sellIn - 1;
+}
+
+int calcNextQuality(Item item, int type){
+    // The next quality value is calculated based on 
+    // current sellIn value
+
+    int nextQuality = item.quality;
+    int modifier = 0;
+
+    // TODO: to determine what is the logic for items belong to more than one type
+    // The logic below assumes every items only belong to one type
+    switch (type) {
+        case ITEM_TYPE_COMMON:
+            modifier = (item.sellIn <= 0) ? -2 : -1;
+            break;
+        case ITEM_TYPE_AGED:
+            modifier = (item.sellIn <= 0) ? 2 : 1;
+            break;
+        case ITEM_TYPE_LEGENDARY:
+            modifier = 0;
+            break;
+        case ITEM_TYPE_PASS:
+            if (item.sellIn <= 0) {
+                nextQuality = 0;
+            }
+            else if (item.sellIn <= 5) {
+                modifier = 3;
+            }
+            else if (item.sellIn <= 10) {
+                modifier = 2;
+            }
+            else {
+                modifier = 1;
+            }
+            break;
+        case ITEM_TYPE_CONJURED:
+            modifier = (item.sellIn <= 0) ? -4 : -2;
+            break;
+    }
+
+    // Calculate next quality
+    nextQuality = nextQuality + modifier;
+  
+    // Make sure next quality does not exceed this range, except legendary items
+    if ((type & ITEM_TYPE_LEGENDARY) == 0) {
+        nextQuality = (nextQuality <= 0) ? 0 : nextQuality;
+        nextQuality = (nextQuality >= 50) ? 50 : nextQuality;
+    }
+    return nextQuality;
+}
+
 void 
 update_quality(Item items[], int size) 
 {
     int i;
+    int itemType;
+    int nextSellIn = 0;
+    int nextQuality = 0;
     
     for (i = 0; i < size; i++)
     {
-        if (strcmp(items[i].name, "Aged Brie") && strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
-        {
-            if (items[i].quality > 0)
-            {
-                if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
-                {
-                    items[i].quality = items[i].quality - 1;
-                }
-            }
-        }
-        else
-        {
-            if (items[i].quality < 50)
-            {
-                items[i].quality = items[i].quality + 1;
-
-                if (!strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
-                {
-                    if (items[i].sellIn < 11)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if (items[i].sellIn < 6)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
-        {
-            items[i].sellIn = items[i].sellIn - 1;
-        }
-
-        if (items[i].sellIn < 0)
-        {
-            if (strcmp(items[i].name, "Aged Brie"))
-            {
-                if (strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
-                {
-                    if (items[i].quality > 0)
-                    {
-                        if (strcmp(items[i].name, "Sulfuras, Hand of Ragnaros"))
-                        {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    items[i].quality = items[i].quality - items[i].quality;
-                }
-            }
-            else
-            {
-                if (items[i].quality < 50)
-                {
-                    items[i].quality = items[i].quality + 1;
-                }
-            }
-        }
+        itemType = getItemType(items[i]);
+        nextSellIn = calcNextSellIn(items[i], itemType);
+        nextQuality = calcNextQuality(items[i], itemType);
+        items[i].sellIn = nextSellIn;
+        items[i].quality = nextQuality;
     }
 }
--- C/GildedRose.h
+++ C/GildedRose.h
@@ -1,6 +1,14 @@
 #ifndef ROSE_INCLUDED
 #define ROSE_INCLUDED
 
+// Define item types here, this is to handle items that 
+// might belong to more than one type e.g. Legendary aged cheese, or conjured backstage pass
+#define ITEM_TYPE_COMMON           0x00
+#define ITEM_TYPE_AGED             0x01
+#define ITEM_TYPE_LEGENDARY        0x02
+#define ITEM_TYPE_PASS             0x04 
+#define ITEM_TYPE_CONJURED         0x08   
+
 typedef struct
 {
     char *name;
@@ -11,5 +19,6 @@ typedef struct
 extern Item* init_item(Item* item, const char *name, int sellIn, int quality);
 extern void update_quality(Item items[], int size);
 extern char* print_item_to_buffer(char* buffer, Item* item);
-
+int getItemType(Item item);
+int getItemTypeFromRegistry(Item item);
 #endif
